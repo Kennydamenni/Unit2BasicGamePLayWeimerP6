a0dd7f13fbe046beec18b15aed9641cf44ed9d57
Kennydamenni <736659@student.dusd.net>
2026-03-13 12:34:46 -0700

    2.2
    yes

--- P6BasicGamePlayweimer/Assets/TutorialInfo/Scripts/PlayerControl.cs
+++ P6BasicGamePlayweimer/Assets/TutorialInfo/Scripts/PlayerControl.cs
@@ -4,7 +4,10 @@ public class PlayerControl : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    
+    public float xRange = 10.0f;
+
+
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +17,23 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Luanch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
